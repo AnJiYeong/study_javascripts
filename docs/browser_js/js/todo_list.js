319c33a041308cbb99f415ab5f7ad868daf4f467
AnJiYeong <memarcis2@naver.com>
2022-11-29 14:57:41 +0900

    221129

--- docs/browser_js/js/todo_list.js
+++ docs/browser_js/js/todo_list.js
@@ -20,24 +20,37 @@ function taskForm(event) {
   }
 }
 
-newTask_start = '<li class="border"><span>';
+newTask_start = '<li class="task"><span>';
 newTask_end = `</span>
                     <span class="flex-container">
                         <span>
-                            <i class="material-icons cursor">favorite_border</i>
+                            <i class="material-icons cursor color-yellow">favorite_border</i>
                         </span>
                         <span>
-                            <i class="material-icons cursor">delete</i>
+                            <i class="material-icons cursor color-red">delete</i>
                         </span>
                     </span>
-                
             </li>`;
 
 function addComment(event) {
-  let taskValue = taskInput.value;
   let queryComments = document.querySelector(".comments");
+  let taskValue = taskInput.value;
   queryComments.insertAdjacentHTML(
     "beforeend",
     newTask_start + taskValue + newTask_end
   );
+  taskInput.value = "";
+  taskInput.focus();
 }
+
+let queryComments = document.querySelector(".comments");
+queryComments.addEventListener("click", (event) => {
+  if (event.target.innerHTML == "favorite_border") {
+    event.target.innerHTML = "favorite";
+  } else if (event.target.innerHTML == "favorite") {
+    event.target.innerHTML = "favorite_border";
+  }
+  if (event.target.innerHTML == "delete") {
+    event.target.parentElement.parentElement.parentElement.remove();
+  }
+});
